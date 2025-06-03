011bbdf117c9269a31bd44343af78ff2bf8d9784
Sagar Admane <sagaradmane6@gmail.com>
2025-06-03 11:40:04 +0530

    new commit 1

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get("/auth/google", passport.authenticate('google', {scope : ['profile', 'em
 
 app.get("/auth/google/callback", passport.authenticate('google', {
     failureRedirect : '/',
-    successRedirect : 'http://localhost:5173/dashboard'
+    successRedirect : 'https://crm-frontend-sage.vercel.app/dashboard'
 }))
 
 app.get('/auth/logout', (req, res) => {
